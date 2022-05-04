919d379e6dcb29502bcb532efefd0ded3bb60a46
Tamiris Vieira <85970510+TamirisVieira@users.noreply.github.com>
2022-05-04 21:22:52 +0000

    ORM

--- index.js
+++ index.js
@@ -12,10 +12,28 @@ app.get("/", async function(req, res){
     res.json(mostrar);
 });
 
-app.post("/", function(req, res){
-    var resultado = usuario.create();
+app.get("/:id", async function(req, res){ 
+  var resultado = await usuario.findAll();
     res.json(resultado);
-})
+});
+
+app.post("/", function(req, res){
+    var resultado = usuario.create(req.body);
+    res.json(resultado);
+});
+
+app.put("/:id", async function(req, res){
+  const resultado = await usuario.findByPk(req.params.id);
+  resultado.nome = "tamiris";
+  const resultadoSave = await resultado.save();
+  res.json(resultadoSave);
+});
+
+app.delete("/:id", async function(req, res){
+    const deletar = await usuario.findByPk(req.params.id);
+    deletar.destroy();
+  res.json(deletar)
+});
 
 app.listen(3000, function(){
     console.log("O servidor está bruto de mais");
