5f8f8442b7c07fbd16b84081633584816e23b83d
Tamiris Vieira <85970510+TamirisVieira@users.noreply.github.com>
2022-05-11 21:23:50 +0000

    Criação do modelo empresas, rotas e migrações, criamos empresas pelo post

--- index.js
+++ index.js
@@ -1,36 +1,66 @@
 var express = require("express");
 var app = express();
-var {usuario} = require("./models");
+var {usuario, empresa} = require("./models");
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }))
 
-app.get("/", async function(req, res){
+app.get("/usuarios", async function(req, res){
     var mostrar = await usuario.findAll();
     res.json(mostrar);
 });
 
-app.get("/:id", async function(req, res){ 
+app.get("/usuarios:id", async function(req, res){ 
   var resultado = await usuario.findAll();
     res.json(resultado);
 });
 
-app.post("/", function(req, res){
+app.post("/usuarios", function(req, res){
     var resultado = usuario.create(req.body);
     res.json(resultado);
 });
 
-app.put("/:id", async function(req, res){
+app.put("/usarios:id", async function(req, res){
   const resultado = await usuario.findByPk(req.params.id);
   resultado.nome = "tamiris";
   const resultadoSave = await resultado.save();
   res.json(resultadoSave);
 });
 
-app.delete("/:id", async function(req, res){
+app.delete("/usuarios:id", async function(req, res){
     const deletar = await usuario.findByPk(req.params.id);
+    deletar.destroy();
+  res.json(deletar)
+});
+
+//empresas
+
+app.get("/empresas", async function(req, res){
+    var mostrar = await empresa.findAll();
+    res.json(mostrar);
+});
+
+app.get("/empresas:id", async function(req, res){ 
+  var resultado = await empresa.findAll();
+    res.json(resultado);
+});
+
+app.post("/empresas", function(req, res){
+    var resultado = empresa.create(req.body);
+    res.json(resultado);
+});
+
+app.put("/empresas:id", async function(req, res){
+  const resultado = await empresa.findByPk(req.params.id);
+  resultado.nome = "ufms";
+  const resultadoSave = await resultado.save();
+  res.json(resultadoSave);
+});
+
+app.delete("/empresas:id", async function(req, res){
+    const deletar = await empresa.findByPk(req.params.id);
     deletar.destroy();
   res.json(deletar)
 });
